48ef3edcdb550fbe2f332c2f8bc89c7f1d9c3245
Luiz Otávio - ADS <luizoscastanheira@hotmail.com.br>
2026-02-17 18:15:29 -0300

    Revistos arquivos crud.js e index.js

--- apiToDo/database/crud.js
+++ apiToDo/database/crud.js
@@ -8,32 +8,32 @@
 
 // Imports e configuração inicial
 import dotenv from 'dotenv'; // Configranção de uso do .env
-dotenv.config({ path: "../.env" }); 
+dotenv.config(); // OBS - não passe o path { path: "../.env" } aqui A nÃO SER que vá testar este aquivo em separado (ex/: node crud.js) para testar as funções, caso contrário deixe no arquivo index
 
 import postgres from "postgres";
 
 // Teste do .env - se der undefined é por porque o .env NÃO foi carregado
-console.log("Host:", process.env.DATABASE_HOST);
-console.log("Port:", process.env.DATABASE_PORT);
+//console.log("Host:", process.env.DATABASE_HOST);
+//console.log("Port:", process.env.DATABASE_PORT);
 
 
 // Conexão com o Banco de Dados
 const sql = postgres({
-      host: process.env.DATABASE_HOST,
-      user: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE,
-      port: Number(process.env.DATABASE_PORT),
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE,
+  port: Number(process.env.DATABASE_PORT),
 });
-
-console.log(sql)
+// Teste básico de conexão
+// console.log(sql)
 
 // const sql = postgres({
-//     host: "localhost",
-//     user: "postgres",
-//     password: "123456",
-//     database: "todo_db",
-//     port: 5432,
+//   host: "localhost",
+//   user: "postgres",
+//   password: "123456",
+//   database: "todo_db",
+//   port: 5432,
 // });
 
 // Teste de conexão ao banco de dados, pode deixar comentado ou apagar
@@ -43,7 +43,7 @@ console.log(sql)
 //     console.log("Conexão bem-sucedida:", result);
 //   } catch (error) {
 //     console.error("Erro na conexão:", error);
-      // evite usar o bloco finally pois ele fecha o pool todo caso vc mantenha função de teste rodando junco com as crud
+// evite usar o bloco finally pois ele fecha o pool todo caso vc mantenha função de teste rodando junco com as crud
 //   } finally {
 //     await sql.end(); // encerra a conexão
 //   }
@@ -54,42 +54,67 @@ console.log(sql)
 
 // Funções para operações com o SGBD
 
-// Inserir um registro
-export async function CreateTodo(title, completed){
-  try{
-    await sql `INSERT INTO todos (title, completed) VALUES (${title}, ${completed});`;
+// CREATE Inserir um registro
+export async function CreateTodo(title, completed) {
+  try {
+    await sql`INSERT INTO todos (title, completed) VALUES (${title}, ${completed});`;
     return 'To-do criada com sucesso';
   } catch {
     return 'Erro ao criar To-do!';
   }
 };
 
-// Listar Todos os registros do banco
-export async function ReadTodos(){
-  return sql  `SELECT id, title, completed FROM todos;`;
-  
+// READ - Listar Todos os registros do banco
+export async function ReadTodos() {
+  return sql`SELECT id, title, created_at completed FROM todos ORDER BY created_at DESC;`;
+
 };
 
 // Listar um registro identificado por um id especificado
-export async function ReadTodosId(id){
-  return sql `SELECT id, title, completed FROM todos WHERE id = ${id};`;
+export async function ReadTodosId(id) {
+  return sql`SELECT id, title, completed FROM todos WHERE id = ${id};`;
 };
-  
-// Atualizando o status de uma to-do através de seu id
-//export async function UpdateTodosStatus(id, completed){
-//  try{
-//    await sql `UPDATE todos set completed = ${ completed } WHERE id = ${ id };`;
-//    return 'To-do concluida.';
-//  } catch {
-//    return 'Erro ao concluir a to-do - não atualizada.';
-//  };
-//};
+
+// UPDATE - Atualizando o status de uma to-do através de seu id
+export async function UpdateTodosStatus(id, completed) {
+  try {
+    await sql`UPDATE todos set completed = ${completed} WHERE id = ${id};`;
+    return 'To-do concluida.';
+  } catch {
+    return 'Erro ao concluir a to-do - não atualizada.';
+  };
+};
+
+// DELETE - Função que apaga um registro
+export async function DeleteTodo(id) {
+  try {
+    await sql`DELETE FROM todos WHERE id = ${id}`;
+    return 'To-do excluido com sucesso'
+  } catch {
+    return 'Erro ao tentar exluir registro.'
+  }
+};
 
 // Teste de função, APAGAR ao concluir o código
-//CreateTodo('Rosicléia', true)
 
+// Função que CRIA um registro
+//await CreateTodo('Mane', true)
+
+// Funções que LEEM registros
 console.log(await ReadTodos());
-console.log(await ReadTodosId(2));
+//console.log(await ReadTodosId(2));
+
+// Função que ATUALIZA um regitro
+//await UpdateTodosStatus(2, false)
+
+// Funções que LEEM registros
+//console.log(await ReadTodos());
+
+// Função que DELETA um registro
+//await DeleteTodo(4);
+
+// Funções que LEEM registros
+//console.log(await ReadTodos());
 
 // encerrando tudo
-await sql.end(); // encerra a conexão
+//await sql.end(); // encerra a conexão
--- apiToDo/index.js
+++ apiToDo/index.js
@@ -3,21 +3,55 @@
  */
 
 // Importações e configurações iniciais
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" }); // passando o caminho para o arquivo .env
+
 import express from "express";
 import cors from "cors";
 
-const app = express();
+const port = process.env.NODE_PORT;
 
+// Teste do .env - se der undefined é por porque o .env NÃO foi carregado
+console.log("Host:", process.env.DATABASE_HOST);
+console.log("Port:", process.env.DATABASE_PORT);
+console.log(port)
+
+import { CreateTodo, UpdateTodosStatus, ReadTodos, ReadTodosId, DeleteTodo } from "./database/crud.js";
+
+// Instanciando no express atribuindo-o à contante app
+const app = express(); 
+
+// Configuranções iniciais para o app
 app.use(express.json()); // habilita o req.body para o formato json
 app.use(cors()); // habilita o cors para aceitar conexões de domínios diferentes
 
+
 // Endpoints
+// Pegando informaçòes do sistema
 app.get("/info", (req, res)=>{
     res.json({"mensagem":"O sistema está online!"});
 });
 
+// READ - Listando todos os registros do banco
+app.get("/todos", async (req, res) => {
+    const Todos = await ReadTodos();
+    res.json(Todos);
+});
+
+// READ - Listando um unico registro pelo id
+app.get("/todos/:id", async (req, res) => {
+    const Todo = await ReadTodosId(req.params.id);
+    res.json(Todo[0])
+});
+
+// CREATE - Cadastrando uma to-do
+app.post("/todos", async (req, res) => {
+    const resposta = await CreateTodo(req.body.title, false);
+    res.json(resposta)
+});
+
 // Configuração de portas
-app.listen(8080, ()=>{
+app.listen(port, ()=>{
     let dataAtual = new Date();
     console.log(`O Servidor iniciou às ${dataAtual}`)
 
